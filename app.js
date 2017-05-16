94adca89da591bb9e90cda9fd9e1018e45236d97
Jamie Wang <sephona@gmail.com>
2017-05-16 18:02:24 -0400

    All working except start over button

--- app.js
+++ app.js
@@ -17,14 +17,26 @@ const quiz = {
       question: "What is the capital of California?",
       answer: ['Sacramento', 'Los Angeles', 'San Diego', 'San Francisco'],
       correctAnswer: 0
+    },
+     {
+      id: 3,
+      question: "What is the capital of North Dakota?",
+      answer: ['Dickinson', 'Fargo', 'Minot', 'Bismark'],
+      correctAnswer: 3
+    },
+     {
+      id: 4,
+      question: "What is the capital of Nevada?",
+      answer: ['Las Vegas', 'Carson City', 'Reno', 'Henderson'],
+      correctAnswer: 1
     }
   ],
 
   userAnswer: [], ///////////REVISIT THIS TO REWORK AS AN ARRAY OF NUMBERS LISTING THE ANSWER CHOICES
   correctAnswers: [],
-  currentQuestion: null,
-  currentAnswerCurrect: true,
-  idNumber: 0
+  currentQuestion: 0, //three diff values, which are null, true, false
+  currentAnswerCorrect: true,
+  idNumber: 0,
 }
 
 //////////////////////////MOD FUNCTIONS//////////////////////////////////
@@ -42,9 +54,9 @@ const quiz = {
   //Note to come back and combine questionsIDIteration and nextQuestion
 
   function nextQuestion(state) {    
-    if(state.idNumber === 3) {
+    if(state.idNumber >= state.questions.length) {
       console.assert('quiz done');
-      // call a function, maybe,to dispaly ansers or ....
+      // call a function, maybe,to display answers or ....
     } else {
       console.assert('quiz not done, next question. Please');
       return state.questions[state.idNumber].question;
@@ -55,101 +67,127 @@ const quiz = {
   function correctAnswerChecker(state, userAnswer, correctAnswer) {
     if(userAnswer === correctAnswer) {
         state.correctAnswers.push(userAnswer);
-        state.currentAnswerCurrect = true;
+        state.currentAnswerCorrect = true;
     } else {
-      state.currentAnswerCurrect = false;
+      state.currentAnswerCorrect = false;
     }
   }
 
-  const render = (state, element = $('div#js-question'), answerCounter = $('div.js-answer-counter'), correctCounter = $('div.js-correct-counter'))=>{
-      const path= state.questions[state.idNumber].answer;
-      const numberOfQuestions = state.questions.length;
-      const numberOfCorrectQuestions = state.correctAnswers.length;//LOOKIE HERE NAEEM!!!
-      const answerBox = `<p>Answered so far: ${state.userAnswer.length}/${numberOfQuestions}</p>`;
-      const correctBox = `<p>Correct so far: ${numberOfCorrectQuestions}/${numberOfQuestions}</p>`;
-       console.log("Correct questions", numberOfCorrectQuestions);
-      const question =  
-        ` <ul>
-          <li>${state.questions[state.idNumber].question}</li>
-            <form id="js-current-question">
-                <p>${path[0]}</p> <input type="radio" name="Answers" value="${path[0]}"><br>
-                <p>${path[1]}</p> <input type="radio" name="Answers" value="${path[1]}"><br>
-                <p>${path[2]}</p> <input type="radio" name="Answers" value="${path[2]}"><br>
-                <p>${path[3]}</p> <input type="radio" name="Answers" value="${path[3]}"> <br>
-                <button type='submit' id="submit">Submit</button>
-            </form>
-        </ul>`;
-     
-
-    element.html(question);
-    answerCounter.html(answerBox);
-    correctCounter.html(correctBox);
+  function currentQuestionMod(state) {
+    if (state.idNumber >= state.questions.length) {
+      state.currentQuestion = 3;
+    }
+    if (state.currentQuestion === 0) {
+      state.currentQuestion = 1;
+    } else if (state.currentQuestion === 1) {
+      state.currentQuestion = 2;
+    } else if (state.currentQuestion === 2) {
+      state.currentQuestion = 1;
+    }
   }
 
 
+ function startOver(state) {
+    state.userAnswer = [];
+    state.correctAnswers = [];
+    state.idNumber = 0;
+    state.currentQuestion = 1;
+  }
 
 
-///////////////////////////////////RENDER COUNTER//////////////////////////////////////////////////
+  const render = (state, element = $('div#js-question'), answerCounter = $('div.js-answer-counter'), 
+      correctCounter = $('div.js-correct-counter'), checkedVal = $('input[name="Answers"]:checked').val()) => {
+      const numberOfQuestions = state.questions.length;
+      const numberOfCorrectQuestions = state.correctAnswers.length;
 
+      if(state.currentQuestion === 3) {
+        const startButton = `<div class="header"><h1>You've completed the quiz!</h1></div>
+        <div><p>Your Score: ${numberOfCorrectQuestions}/${numberOfQuestions}</p></div>
+        <div id="startover">
+        <button class="start-over-button">Start Over</button>
+        </div>
+      `;
+       $('div.js-answer-counter').addClass('hidden');
+       $('div.js-correct-counter').addClass('hidden');
+      
+      
+        element.html(startButton);
 
+      } else if (state.currentQuestion === 0) {
+        const startButton = `<div class="start-button"><button>Start</button></div>`;
+        $('div.js-answer-counter').addClass('hidden');
+        $('div.js-correct-counter').addClass('hidden');
+        element.html(startButton);
 
-  const renderCounters = (state, answerCounter = $('div.js-answer-counter'), 
-    correctCounter = $('div.js-correct-counter'), checkedVal = $('input[name="Answers"]:checked').val()) => {
+      } else if (state.currentQuestion === 1) {
+          $('div.js-answer-counter').removeClass('hidden');
+          $('div.js-correct-counter').removeClass('hidden');
+          const path= state.questions[state.idNumber].answer;
+          //LOOKIE HERE NAEEM!!!
+          const answerBox = `<p>Answered so far: ${state.userAnswer.length}/${numberOfQuestions}</p>`;
+          const correctBox = `<p>Correct so far: ${numberOfCorrectQuestions}/${numberOfQuestions}</p>`;
+          console.log("Correct questions", numberOfCorrectQuestions);
+          const question =  
+            ` <ul>
+              <li>${state.questions[state.idNumber].question}</li>
+                <form id="js-current-question">
+                    <p>${path[0]}</p> <input type="radio" name="Answers" value="${path[0]}"><br>
+                    <p>${path[1]}</p> <input type="radio" name="Answers" value="${path[1]}"><br>
+                    <p>${path[2]}</p> <input type="radio" name="Answers" value="${path[2]}"><br>
+                    <p>${path[3]}</p> <input type="radio" name="Answers" value="${path[3]}"> <br>
+                    <button type='submit' id="submit">Submit</button>
+                </form>
+            </ul>`;
+        element.html(question);
+        answerCounter.html(answerBox);
+        correctCounter.html(correctBox);   
 
-    const numberOfQuestions = state.questions.length;
-    const numberOfCorrectQuestions = state.correctAnswers.length;
-    let correctAnsIndex = state.questions[state.idNumber].correctAnswer;
-    let correctAnsVal = state.questions[state.idNumber].answer[correctAnsIndex];
-    const answerBox = `<p>Answered so far: ${state.userAnswer.length}/${numberOfQuestions}</p>`;
-    const correctBox = `<p>Correct so far: ${numberOfCorrectQuestions}/${numberOfQuestions}</p>`;
+      } else if (state.currentQuestion === 2) {
+          const numberOfQuestions = state.questions.length;
+          const numberOfCorrectQuestions = state.correctAnswers.length;
+          let correctAnsIndex = state.questions[state.idNumber].correctAnswer;
+          let correctAnsVal = state.questions[state.idNumber].answer[correctAnsIndex];
+          const answerBox = `<p>Answered so far: ${state.userAnswer.length}/${numberOfQuestions}</p>`;
+          const correctBox = `<p>Correct so far: ${numberOfCorrectQuestions}/${numberOfQuestions}</p>`;
 
-      // let answer = correctAnswerChecker(quiz, checkedVal, correctAnsVal);
-      if(state.currentAnswerCurrect === true) {
-        $('#js-feedback').append(`<p><strong>Correct</strong></p>`);
-      } else {
-        $('#js-feedback').append(`<p><strong>Incorrect</strong></p>`);
+            // let answer = correctAnswerChecker(quiz, checkedVal, correctAnsVal);
+            if(state.currentAnswerCorrect === true) {
+              $('#js-feedback').append(`<p><strong>Correct</strong></p>`);
+            } else {
+              $('#js-feedback').append(`<p><strong>Incorrect</strong></p>`);
+            }
+
+          $('#js-current-question').find('> p').addClass('red');
+          $('#js-current-question p').eq(correctAnsIndex).addClass('green');
+          //.css('color', 'green');//this also works when replacing addClass on line 135
+          // console.log($('form#js-current-question p').eq(correctAnsIndex).text());
+          $('#js-current-question').find('button').addClass('hidden');
+          $('#js-feedback').append(`<form id="js-next"><button type='submit'>Next Question</button></form>`);
+          answerCounter.html(answerBox);   
+          correctCounter.html(correctBox);
+              console.log("Correct questions", numberOfCorrectQuestions);
       }
 
-    $('#js-current-question').find('> p').addClass('red');
-    $('#js-current-question p').eq(correctAnsIndex).addClass('green');
-    //.css('color', 'green');//this also works when replacing addClass on line 135
-    // console.log($('form#js-current-question p').eq(correctAnsIndex).text());
-    $('#js-current-question').find('button').addClass('hidden');
-
-
-    $('#js-feedback').append(`<form id="js-next"><button type='submit'>Next Question</button></form>`);
-    answerCounter.html(answerBox);   
-    correctCounter.html(correctBox);
-        console.log("Correct questions", numberOfCorrectQuestions);
   }
 
 
-// console.log('quiz', selectAnswer(0), quiz.userAnswer);
-// selectAnswer(1);
-// console.log(nextQuestion());
-// selectAnswer(0);
-// // questionsIdIteration();
-// console.log(nextQuestion());
-// console.log(quiz.userAnswer);
 
-//////////////////RUN ON LOAD OF PAGE///////////////////
-//After submit
-  //The value of the answer is pushed to userAnswers
-  //The correct answer to be green
-    //use filter to addClass (css of font-color green)
-  //The wrong answers to be red (DO THIS FIRST)
-    //use map to addClass (css of font-color red)
-  //Put conditional statement -- goes into div #js-feedback
-    //if the index of the correctanswer equals the user answer then we say
-      //Correct!
-    //else we say Incorrect
-//Update Answered so far
-//Update Correct so far
-//Pop up next question button
-
-$(function(){
+$(function() {
 
     render(quiz);
+
+
+ $('div.header').find('button').click((event) => {
+    console.log('there');
+      startOver(quiz);
+      render(quiz);
+    });
+
+//////////////////////////START BUTTON EVENT LISTNER////////////////////
+    $('.start-button').click('button', (event) => {
+      currentQuestionMod(quiz);
+      render(quiz);
+    });
 
 ///////////////////SUBMIT EVENT LISTENER/////////////////////////
     $('#js-question').on('submit', '#js-current-question', event =>{ //if you run into issues, change .submit to .on(submit)
@@ -159,7 +197,8 @@ $(function(){
       let correctAnsVal = quiz.questions[quiz.idNumber].answer[correctAnsIndex];
       selectAnswer(quiz, checkedVal);
       correctAnswerChecker(quiz, checkedVal, correctAnsVal);
-      renderCounters(quiz);
+      currentQuestionMod(quiz);
+      render(quiz);
       questionsIdIteration(quiz);
       console.log(quiz.correctAnswers);
     });
@@ -170,21 +209,21 @@ $(function(){
       
       console.log('that', quiz.idNumber);
       console.log(quiz.questions.length);
-      if(quiz.idNumber !== quiz.questions.length) {
-          console.log('quiz not done, next question. Please');
-          nextQuestion(quiz);
-          render(quiz);
-        } else {
-          console.log('quiz done');
+    
+      nextQuestion(quiz);
+      currentQuestionMod(quiz);
+      render(quiz);
+      console.log('quiz done');
           // call a function, maybe,to display ansers or ....
           // when done , can do a .remove() on js-feedback. and count right answers.
-        }
+        
       
       $('#js-feedback').html('');
     });
 
 
 ///////////////////////////////////LAST PAGE (OUTRO)////////////////////////////
-    
+ 
+
 })
 
