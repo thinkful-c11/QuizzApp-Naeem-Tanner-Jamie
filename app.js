8d225c8b4d3c26406c769a1713d336c6808f2641
Jamie Wang <sephona@gmail.com>
2017-05-16 10:53:29 -0400

    Have all features done except outro page

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const quiz = {
 //////////////////////////MOD FUNCTIONS//////////////////////////////////
 
   function selectAnswer(answer){
-   return quiz.userAnswer.push(answer);
+   quiz.userAnswer.push(answer);
   }
 
   let idNumber = 0;
@@ -41,13 +41,23 @@ const quiz = {
 
   //Note to come back and combine questionsIDIteration and nextQuestion
 
-  function nextQuestion(){
+  function nextQuestion() {    
+    if(idNumber === 3) {
+      console.assert('quiz done');
+      // call a function, maybe,to dispaly ansers or ....
+    } else {
+      console.assert('quiz not done, next question. Please');
       return quiz.questions[idNumber].question;
+    }
+     
   }
 
   function correctAnswerChecker(userAnswer, correctAnswer) {
     if(userAnswer === correctAnswer) {
         quiz.correctAnswers.push(userAnswer);
+        return true;
+    } else {
+      return false;
     }
   }
 
@@ -57,9 +67,9 @@ const quiz = {
                  state = quiz)=>{
       const path= state.questions[idNumber].answer;
       const numberOfQuestions = quiz.questions.length;
-      const numberOfCorrectQuestions = quiz.correctAnswers.length;
+      const numberOfCorrectQuestions = quiz.correctAnswers.length;//LOOKIE HERE NAEEM!!!
       const answerBox = `<p>Answered so far: ${idNumber}/${numberOfQuestions}</p>`;
-      const correctBox = `<p>Correct so far: ${numberOfCorrectQuestions}/${numberOfQuestions}</p>`
+      const correctBox = `<p>Correct so far: ${numberOfCorrectQuestions}/${numberOfQuestions}</p>`;
       const question =  
         ` <ul>
           <li>${state.questions[idNumber].question}</li>
@@ -76,13 +86,25 @@ const quiz = {
     correctCounter.html(correctBox);
   }
 
+  const renderCounters = (answerCounter = $('div.js-answer-counter'),
+                 correctCounter = $('div.js-correct-counter'), 
+                 state = quiz) => {
+    const numberOfQuestions = state.questions.length;
+    const numberOfCorrectQuestions = state.correctAnswers.length;
+    const answerBox = `<p>Answered so far: ${idNumber}/${numberOfQuestions}</p>`;
+    const correctBox = `<p>Correct so far: ${numberOfCorrectQuestions}/${numberOfQuestions}</p>`;
+    answerCounter.html(answerBox);
+    correctCounter.html(correctBox);
+  }
+
+
 // console.log('quiz', selectAnswer(0), quiz.userAnswer);
-selectAnswer(1);
-console.log(nextQuestion());
-selectAnswer(0);
-// questionsIdIteration();
-console.log(nextQuestion());
-console.log(quiz.userAnswer);
+// selectAnswer(1);
+// console.log(nextQuestion());
+// selectAnswer(0);
+// // questionsIdIteration();
+// console.log(nextQuestion());
+// console.log(quiz.userAnswer);
 
 //////////////////RUN ON LOAD OF PAGE///////////////////
 //After submit
@@ -101,27 +123,75 @@ console.log(quiz.userAnswer);
 
 $(function(){
 
-render();
+    render();
 
-$('form#js-current-question').submit((event)=>{ //if you run into issues, change .submit to .on(submit)
-  event.preventDefault();
-  let checkedVal = $('input[name="Answers"]:checked').val();
-  selectAnswer(checkedVal);
-  let correctAnsIndex = quiz.questions[idNumber].correctAnswer;
-  let correctAnsVal = quiz.questions[idNumber].answer[correctAnsIndex];
-  correctAnswerChecker(checkedVal, correctAnsVal);
-  //pop up window (alert?) that says 'Correct' 
-//   console.log(quiz.userAnswer);
-//   console.log(quiz.correctAnswers);
+///////////////////SUBMIT EVENT LISTENER/////////////////////////
+    $('#js-question').on('submit', '#js-current-question', (event)=>{ //if you run into issues, change .submit to .on(submit)
+      event.preventDefault();
+      let checkedVal = $('input[name="Answers"]:checked').val();
+      selectAnswer(checkedVal);
 
-questionsIdIteration();
-nextQuestion();
-render();
+      let correctAnsIndex = quiz.questions[idNumber].correctAnswer;
+      let correctAnsVal = quiz.questions[idNumber].answer[correctAnsIndex];
+      console.log(checkedVal + ', ' + correctAnsVal);
+      //I don't know why it's pushing the checkedVal twice into the correctAnswers
 
-  console.log(quiz.userAnswer);
-  console.log(quiz.correctAnswers);
+      var answer = correctAnswerChecker(checkedVal, correctAnsVal);
+      // console.info('answer ' , answer);
+      if(answer === true) {
+        $('#js-feedback').append(`<p><strong>Correct</strong></p>`);
+      } else {
+        $('#js-feedback').append(`<p><strong>Incorrect</strong></p>`);
+      }
 
-})
+    $('#js-current-question').find('> p').addClass('red');
+    $('#js-current-question p').eq(correctAnsIndex).addClass('green');
+    //.css('color', 'green');//this also works when replacing addClass on line 135
+    // console.log($('form#js-current-question p').eq(correctAnsIndex).text());
+    $('#js-current-question').find('button').addClass('hidden');
+
+
+    $('#js-feedback').append(`<form id="js-next"><button type='submit'>Next Question</button></form>`);
+
+    questionsIdIteration();
+    // const numberOfQuestions = quiz.questions.length;
+    // const numberOfCorrectQuestions = quiz.correctAnswers.length;
+    // const answerBox = `<p>Answered so far: ${idNumber}/${numberOfQuestions}</p>`;
+    // const correctBox = `<p>Correct so far: ${numberOfCorrectQuestions}/${numberOfQuestions}</p>`;
+    // $('div.js-answer-counter').html(answerBox);
+    // $('div.js-correct-counter').html(correctBox);
+
+    renderCounters();
+
+
+      //pop up window (alert?) that says 'Correct' 
+    //   console.log(quiz.userAnswer);
+    //   console.log(quiz.correctAnswers);
+      console.log(quiz.correctAnswers);
+
+    // questionsIdIteration();
+    // nextQuestion();
+    // render();
+    });
+
+///////////////////NEXT QUESTION EVENT LISTENER/////////////////////////
+    $('#js-feedback').on('submit', '#js-next', (event) => {
+      event.preventDefault();
+      // var idCheck = questionsIdIteration();
+      console.log(idNumber);
+      console.log(quiz.questions.length);
+      if(idNumber !== quiz.questions.length) {
+          console.log('quiz not done, next question. Please');
+          nextQuestion();
+          render();
+        } else {
+          console.log('quiz done');
+          // call a function, maybe,to display ansers or ....
+          // when done , can do a .remove() on js-feedback. and count right answers.
+        }
+      
+      $('#js-feedback').html('');
+    });
 
     
 })
